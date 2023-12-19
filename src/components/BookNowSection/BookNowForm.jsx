3f8803b5ed511cb2cae514bc05151207a3a7b796
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-12-19 12:06:31 +0600

    completed book now form

--- src/components/BookNowSection/BookNowForm.jsx
+++ src/components/BookNowSection/BookNowForm.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import style from "./book-now.module.css";
 const BookNowForm = () => {
+  const [star, setStar] = useState(0);
+  const [room, setRoom] = useState(0);
   return (
     <div className={style.form_container}>
       <h1>Book now</h1>
@@ -13,24 +16,34 @@ const BookNowForm = () => {
         </label>
 
         <div className={style.double_col}>
-          <label htmlFor="city">
-            <p>CITY</p>
-            <input type="text" name="city" placeholder="Enter Your City Here" />
+          <label htmlFor="arrival">
+            <p>Arrival</p>
+            <input type="text" name="arrival" placeholder="10 October" />
           </label>
-          <label htmlFor="city">
-            <p>CITY</p>
-            <input type="text" name="city" placeholder="Enter Your City Here" />
+
+          <label htmlFor="Departure">
+            <p>Departure</p>
+            <input type="text" name="Departure" placeholder="11 October" />
           </label>
         </div>
+
         <div className={style.double_col}>
-          <label htmlFor="city">
-            <p>CITY</p>
-            <input type="text" name="city" placeholder="Enter Your City Here" />
-          </label>
-          <label htmlFor="city">
-            <p>CITY</p>
-            <input type="text" name="city" placeholder="Enter Your City Here" />
-          </label>
+          <div className={style.increaseInput}>
+            <p>STAR</p>
+            <div className="">
+              <button onClick={() => setStar(star - 1)}>-</button>
+              {star}
+              <button onClick={() => setStar(star + 1)}>+</button>
+            </div>
+          </div>
+          <div className={style.increaseInput}>
+            <p>Room</p>
+            <div className="">
+              <button onClick={() => setRoom(room - 1)}>-</button>
+              {room}
+              <button onClick={() => setRoom(room + 1)}>+</button>
+            </div>
+          </div>
         </div>
 
         <button className={style.submitButton}>Book Now</button>
