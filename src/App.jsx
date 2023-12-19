532b0066192ea05fb60c62772f2a28fcd71d9cbd
CodeWithIftekhar <codewithiftekhar@gmail.com>
2023-12-19 12:14:20 +0600

    change the meta tag and added loader spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import DiscountSection from "./DiscountSection/DiscountSection";
 import BookNowSection from "./components/BookNowSection/BookNowSection";
 import Footer from "./components/Footer/Footer";
@@ -6,19 +7,34 @@ import Navbar from "./components/Navbar/Navbar";
 import OurDestinationSection from "./components/OurDestinationSection/OurDestinationSection";
 import StudentReviewSection from "./components/StudentReviewSection/StudentReviewSection";
 import TestimonialsSection from "./components/TestimonialsSection/TestimonialsSection";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
-    <div>
-      <Navbar />
-      <HeroSection />
-      <StudentReviewSection />
-      <OurDestinationSection />
-      <DiscountSection />
-      <BookNowSection />
-      <TestimonialsSection />
-      <Footer />
-    </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Navbar />
+          <HeroSection />
+          <StudentReviewSection />
+          <OurDestinationSection />
+          <DiscountSection />
+          <BookNowSection />
+          <TestimonialsSection />
+          <Footer />
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,10 @@
+import style from "./loader.module.css";
+const Loader = () => {
+  return (
+    <div id={style.loadessr}>
+      <div id={style.loader}></div>
+    </div>
+  );
+};
+
+export default Loader;
